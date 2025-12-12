d6e295c2f2602713c5cadd170162471b7c662c36
Israel Santos Lima <israellima@dcc.ufmg.br>
2025-12-12 14:35:28 -0300

    first commit

---
+++ src/main/java/com/business/project/ms_comunicacao/MsComunicacaoApplication.java
@@ -0,0 +1,13 @@
+package com.business.project.ms_comunicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsComunicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsComunicacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/business/project/ms_comunicacao/MsComunicacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.project.ms_comunicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsComunicacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
